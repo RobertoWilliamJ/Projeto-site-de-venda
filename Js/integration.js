d1e7732e5d9eea622d1830df60c597d6d2fbe92b
RobertoWilliamJ <140459931+RobertoWilliamJ@users.noreply.github.com>
2023-11-07 18:08:16 -0300

    alteraçoes

--- Js/integration.js
+++ Js/integration.js
@@ -1,10 +1,35 @@
 
-function myFunction(imgs) {
-    var expandImg = document.getElementById("expandedImg");
-    var imgText = document.getElementById("imgtext");
-    expandImg.src = imgs.src;
-    imgText.innerHTML = imgs.alt;
-    expandImg.parentElement.style.display = "block";
- }
+//function myFunction(imgs) {
+ //   var expandImg = document.getElementById("expandedImg");
+ //   var imgText = document.getElementById("imgtext");
+ //   expandImg.src = imgs.src;
+ //   imgText.innerHTML = imgs.alt;
+//    expandImg.parentElement.style.display = "block";
+// }
   
 
+let currentImageIndex = 0;
+
+function openImage(img) {
+    const expandImg = document.getElementById("expandedImg");
+    const imgText = document.getElementById("imgtext");
+    const images = img.parentElement.parentElement.querySelectorAll(".coluna img");
+    const alt = img.alt;
+
+    currentImageIndex = Array.from(images).indexOf(img);
+
+    expandImg.src = img.src;
+    imgText.innerHTML = alt;
+}
+
+function nextImage() {
+    const images = document.querySelectorAll(".coluna img");
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    openImage(images[currentImageIndex]);
+}
+
+function prevImage() {
+    const images = document.querySelectorAll(".coluna img");
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    openImage(images[currentImageIndex]);
+}

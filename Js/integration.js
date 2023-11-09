31072c92baee50823cbf9ff813ebc37317b16b71
RobertoWilliamJ <140459931+RobertoWilliamJ@users.noreply.github.com>
2023-11-09 14:07:23 -0300

    finalizado

--- Js/integration.js
+++ Js/integration.js
@@ -9,27 +9,61 @@
   
 
 let currentImageIndex = 0;
+//
+//function openImage(img) {
+//    const expandImg = document.getElementById("expandedImg");
+//    const imgText = document.getElementById("imgtext");
+//    const images = img.parentElement.parentElement.querySelectorAll(".coluna img");
+//    const alt = img.alt;
 
-function openImage(img) {
-    const expandImg = document.getElementById("expandedImg");
-    const imgText = document.getElementById("imgtext");
-    const images = img.parentElement.parentElement.querySelectorAll(".coluna img");
-    const alt = img.alt;
+//    currentImageIndex = Array.from(images).indexOf(img);
 
-    currentImageIndex = Array.from(images).indexOf(img);
+ //   expandImg.src = img.src;
+ //   imgText.innerHTML = alt;
+//}
 
-    expandImg.src = img.src;
-    imgText.innerHTML = alt;
+//function nextImage() {
+ //   const images = document.querySelectorAll(".coluna img");
+ //   currentImageIndex = (currentImageIndex + 1) % images.length;
+ //   openImage(images[currentImageIndex]);
+//}
+
+//function prevImage() {
+//    const images = document.querySelectorAll(".coluna img");
+ //   currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+ //   openImage(images[currentImageIndex]);
+//}
+
+
+//botoes
+
+function showImage(albumId) {
+    const album = document.getElementById(albumId);
+    const images = album.getElementsByTagName('img');
+
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = 'none';
+    }
+
+    images[currentImageIndex].style.display = 'block';
 }
 
-function nextImage() {
-    const images = document.querySelectorAll(".coluna img");
+function nextImage(albumId) {
+    const album = document.getElementById(albumId);
+    const images = album.getElementsByTagName('img');
+
     currentImageIndex = (currentImageIndex + 1) % images.length;
-    openImage(images[currentImageIndex]);
+    showImage(albumId);
 }
 
-function prevImage() {
-    const images = document.querySelectorAll(".coluna img");
+function prevImage(albumId) {
+    const album = document.getElementById(albumId);
+    const images = album.getElementsByTagName('img');
+
     currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-    openImage(images[currentImageIndex]);
+    showImage(albumId);
 }
+
+showImage('album1')
+showImage('album2')
+showImage('album3')
